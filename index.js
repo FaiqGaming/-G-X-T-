3c1a6a793c7452552445c7c62972a6a1d54121a5
AnakBodohKaliaan <39037579+FaiqGaming@users.noreply.github.com>
2018-12-31 19:19:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ client.on('ready', async () => {
 })
 
 client.on("message", async message => {
+       	if (message.author.bot) return; 
+	if (message.channel.type === "dm") return;
+
 	if(!message.content.startsWith(prefix)) return;
         let messageArray = message.content.split(" ");
         let sender = message.author;
